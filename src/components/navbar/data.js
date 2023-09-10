8cc2df5f2ffb2effb8583dc92cef644edbeaa237
Arthur Smart <89918737+Arthur-Smart@users.noreply.github.com>
2023-09-10 17:32:42 +0300

    Update data.js

--- src/components/navbar/data.js
+++ src/components/navbar/data.js
@@ -1,25 +1,25 @@
 export const Icons = [
     {
         id:1,
-        img: <i class="fa-brands fa-square-facebook"></i>,
+        img: <i class="fa-brands fa-square-facebook fa-2x"></i>,
         link:"https://facebook.com",
         title:"facebook"
     },
      {
         id:2,
-        img: <i class="fa-brands fa-square-instagram "></i>,
+        img: <i class="fa-brands fa-square-instagram fa-2x"></i>,
         link:"https://instagram.com",
         title:"facebook"
     },
      {
         id:1,
-        img: <i class="fa-brands fa-square-twitter"></i>,
+        img: <i class="fa-brands fa-square-twitter fa-2x"></i>,
         link:"https://twitter.com",
         title:"facebook"
     },
      {
         id:1,
-        img: <i class="fa-brands fa-square-youtube"></i>,
+        img: <i class="fa-brands fa-square-youtube fa-2x"></i>,
         link:"https://youtube.com",
         title:"facebook"
     }
